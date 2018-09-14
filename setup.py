4fd969b54461cc424b343bb6db98661290e45eb0
Lyubomir Rumenov <lubeto3099@gmail.com>
2018-09-14 20:00:33 +0300

    Update to version 0.2

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
   name='RPi.GPIO-def',
   packages=['RPi.GPIO', 'RPi.GPIO.definitions', 'RPi.GPIO.definitions.PWM', 'RPi.GPIO.definitions.functions'],
-  version='0.1.1-alpha',
+  version='0.2',
   description='Definition library for RPi.GPIO(used for IntelliSense)',
   author='Lyubomir Rumenov',
-  author_email='lubeto_9@mail.bg',
+  author_email='lubeto3099@gmail.com',
   url='https://github.com/Def4l71diot/RPi.GPIO-def',
-  download_url='https://github.com/Def4l71diot/RPi.GPIO-def/tarball/0.1.1-alpha',
+  download_url='https://github.com/Def4l71diot/RPi.GPIO-def/tarball/0.2',
   keywords=['raspberry', 'gpio', 'RPi.GPIO', 'definition', 'windows', 'linux', 'RPi', 'GPIO'],
   classifiers=[],
 )
